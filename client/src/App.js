790ca69b3159e334ba7f8224cc7bde2e77c282ce
nintichance <mintidance@gmail.com>
2018-01-23 11:51:03 -0500

    Refactored async/await syntax to have a catch

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,19 @@ import './App.css'
 import axios from 'axios'
 
 class App extends Component {
-  componentWillMount() {
 
+  async componentWillMount() {
     //Call to Express API for user data from MongoDB
-    const getUserData = async () => {
-      const data = await axios.get('http://localhost:4000/api/users')
-      document.window.append(data)
+    try {
+      const res = await axios.get('http://localhost:4000/api/users')
+      const users = res.data
+      document.window.append(users)
+    }
+    catch(err) {
+      console.error(err)
     }
   }
+
   render() {
     return (
       <div className="App">
