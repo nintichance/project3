94e84e1a38f65172dc6bf3fdc8fded71c91bdaae
nintichance <mintidance@gmail.com>
2018-01-23 11:10:07 -0500

    Made axios call to User data from React

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
 class App extends Component {
- 
+  componentWillMount() {
+    //Call to API for user data
+    axios.get('http://localhost:4000/api/users'
+    )
+      .then(response => {
+        const data = response.data
+        document.window.append(data);
+      })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Hello
       </div>
     );
   }
