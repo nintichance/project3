8a64bac88eeb2fbe4a0d4433537b6ffcbb490967
nintichance <mintidance@gmail.com>
2018-01-23 11:14:22 -0500

    Changed promise syntax from .then to async/await syntax

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,20 @@ import axios from 'axios'
 
 class App extends Component {
   componentWillMount() {
-    //Call to API for user data
-    axios.get('http://localhost:4000/api/users'
-    )
-      .then(response => {
-        const data = response.data
-        document.window.append(data);
-      })
+
+    //Call to Express API for user data from MongoDB
+    const getUserData = async () => {
+      const data = await axios.get('http://localhost:4000/api/users')
+      document.window.append(data)
+    }
   }
   render() {
     return (
       <div className="App">
         Hello
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
