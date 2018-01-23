8db8d8a3b55702740c171b3f139c0021924b28aa
nintichance <mintidance@gmail.com>
2018-01-23 15:22:28 -0500

    Set up basic UserPage Component

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,9 +1,16 @@
-// import React from 'react'
-// import UserList from './UserList'
+import React from 'react'
+import UserList from './UserList'
+
+const UserPage = () => {
+    return(
+        <div>
+            Hello from UserPage
+            <UserList />
+        </div>
+    )
+}
 
 
 
 
-
-
-//  export default UserPage
+ export default UserPage
