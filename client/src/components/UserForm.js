b7fd17446eed09a9352342c6c4602ca9f3fbd5dc
nintichance <mintidance@gmail.com>
2018-01-23 21:42:51 -0500

    Form added to UserForm and needs to be functional

--- client/src/components/UserForm.js
+++ client/src/components/UserForm.js
@@ -4,7 +4,7 @@ class UserForm extends Component {
     constructor(props) {
         super(props)
         this.defaultState = {
-            user: {
+            newUser: {
                 firstName: 'Eric',
                 lastName: 'Lu',
                 img: 'https://i.imgur.com/KIiCMv3.jpg'
@@ -36,32 +36,32 @@ class UserForm extends Component {
         return (
             <div>
                 Hello from UserForm
-                {/* <form onSubmit={this._addNewCredit}>
+                <form onSubmit={this._addNewUser}>
                     <div>
                         <span>New User </span>
                         <input
                             type="string"
                             name="firstName"
                             placeholder="First Name"
-                            value={this.state.user.firstName}
+                            value={this.state.newUser.firstName}
                             onChange={this.handleInputChange} />
                     </div>
 
                     <div>
                         
                         <input
-                            type="text"
-                            name="description"
-                            placeholder="Description"
-                            value={this.state.newCredit.description}
+                            type="string"
+                            name="lastName"
+                            placeholder="Last Name"
+                            value={this.state.newUser.lastName}
                             onChange={this.handleInputChange} />
                     </div>
                     <div>
                         <input
-                            type="text"
-                            name="description"
-                            placeholder="Description"
-                            value={this.state.newCredit.description}
+                            type="string"
+                            name="img"
+                            placeholder="Image URL"
+                            value={this.state.newUser.img}
                             onChange={this.handleInputChange} />
                     </div>
                     <div>
@@ -70,7 +70,7 @@ class UserForm extends Component {
                             value="Add New User" />
                     </div>
 
-                </form> */}
+                </form>
             </div>
         )
 
