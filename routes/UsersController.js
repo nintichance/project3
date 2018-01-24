4216b8128803e218f5da3f049ac068ed082258ab
nintichance <mintidance@gmail.com>
2018-01-23 20:47:22 -0500

    Refactored UserController and added new user route

--- routes/UsersController.js
+++ routes/UsersController.js
@@ -2,14 +2,25 @@ const express = require('express')
 const User = require('../db/models/User')
 const router = express.Router()
 
-router.get('/', (req, res)=>{
-    User.find({})
-        .then((users)=>{
-            res.json(users)
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
+router.get('/', async(req, res)=>{
+    const users = await User.find({}) 
+    try{
+        res.json(users)
+    } 
+    catch(err){
+        console.log(err)
+    }
+})
+
+router.get('/', async(req, res)=>{
+    const newUser = new User(req.body.user)
+    const user = await newUser.save() 
+    try{
+        res.json(user)
+    } 
+    catch(err){
+        console.log(err)
+    }
 })
 
 module.exports = router
