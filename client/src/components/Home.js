53b24df30cff9e0447c383ae9b50e628c2ce644f
nintichance <mintidance@gmail.com>
2018-01-27 14:50:36 -0500

    Added animated gradient background to AppComponent and NavBar to Home Component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { HomeContainer } from './styled-components/Containers'
+import NavBar from './NavBar'
 
 
 
 const Home = () => {
     return(
         <HomeContainer>
+            <NavBar />
             Hello from Home
             <Link to='/users'>Users</Link>
         </HomeContainer>
--- client/src/components/styled-components/Containers.js
+++ client/src/components/styled-components/Containers.js
@@ -2,9 +2,32 @@ import styled from 'styled-components'
 
 
 export const Container = styled.div`
-    display: grid;
+    display: flex;
+    flex-direction: column;
     justify-items: center;
     width: 100vw;
+    background: linear-gradient(270deg, #17d1a1, #d9a5ef);
+    background-size: 400% 400%;
+  
+  -webkit-animation: AnimationName 15s ease infinite;
+  -moz-animation: AnimationName 15s ease infinite;
+  animation: AnimationName 15s ease infinite;
+  
+  @-webkit-keyframes AnimationName {
+      0%{background-position:0% 50%}
+      50%{background-position:100% 50%}
+      100%{background-position:0% 50%}
+  }
+  @-moz-keyframes AnimationName {
+      0%{background-position:0% 50%}
+      50%{background-position:100% 50%}
+      100%{background-position:0% 50%}
+  }
+  @keyframes AnimationName { 
+      0%{background-position:0% 50%}
+      50%{background-position:100% 50%}
+      100%{background-position:0% 50%}
+  }
    
 `
 export const UserShowContainer = Container.extend`
@@ -17,8 +40,9 @@ export const UserContainer = styled.div`
    
 `
 export const  HomeContainer = Container.extend`
-  display: grid;
   justify-items: center;
+  height: 100vh;
+  width: 100 vw;
 `
 export const UserListContainer = Container.extend`
   
